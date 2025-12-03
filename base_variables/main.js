fab24332fac18f10a3ce29a6f9eb11fa65468707
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-03 09:33:29 -0400

    Add variable declarations and console log statements in main.js

--- base_variables/main.js
+++ base_variables/main.js
@@ -1,3 +1,9 @@
 var myName = "Javier";
-console.log("Hello, " + myName + "!");
+var myAge = 55;
+var isDeveloper = true;
+var month;
+var variableNull = null;
+console.log("Initial Month is: ".concat(month));
+month = "June";
+console.log("New Month is: ".concat(month));
 console.log("Hello, ".concat(myName, "!"));
