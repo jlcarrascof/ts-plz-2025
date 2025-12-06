2543fc82a25c22f3f4d64be4143b8f0b913c63ea
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-06 19:40:37 -0400

    Add throwError function to handle error messages in main.ts

--- different_variables/main.ts
+++ different_variables/main.ts
@@ -13,3 +13,9 @@ variableUnknown = 42;
 if (typeof variableUnknown === 'string') {
     console.log('variableUnknown is a string:', variableUnknown);
 }
+
+function throwError(message: string): never {
+    throw new Error(message);
+}
+
+throwError('This is an error message');
