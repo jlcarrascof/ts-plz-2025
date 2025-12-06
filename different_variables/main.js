729dcf54a66b2615208138a0a2c19a96eb04c320
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-05 22:55:43 -0400

    Refactor variable assignments in main.js and main.ts for clarity

--- different_variables/main.js
+++ different_variables/main.js
@@ -3,3 +3,10 @@ var variableAny;
 variableAny = 42;
 // variableAny = true;
 console.log('Value of variableAny is:', variableAny);
+var variableUnknown;
+variableUnknown = "Hello";
+// variableUnknown = 42;
+// variableUnknown = true;
+if (typeof variableUnknown === 'string') {
+    console.log('variableUnknown is a string:', variableUnknown);
+}
--- different_variables/main.ts
+++ different_variables/main.ts
@@ -7,8 +7,8 @@ console.log('Value of variableAny is:', variableAny);
 let variableUnknown: unknown;
 
 variableUnknown = "Hello";
-variableUnknown = 42;
-variableUnknown = true;
+// variableUnknown = 42;
+// variableUnknown = true;
 
 if (typeof variableUnknown === 'string') {
     console.log('variableUnknown is a string:', variableUnknown);
