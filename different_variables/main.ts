0490c5c7cd81ef791b537569ac78b07def5a4769
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-05 22:51:20 -0400

    Update variableAny assignment in main.ts and add console log for variableUnknown

--- different_variables/main.ts
+++ different_variables/main.ts
@@ -1,5 +1,13 @@
 let variableAny: any;
 // variableAny = "Hello";
-// variableAny = 42;
+variableAny = 42;
 // variableAny = true;
 console.log('Value of variableAny is:', variableAny);
+
+let variableUnknown: unknown;
+
+variableUnknown = "Hello";
+variableUnknown = 42;
+variableUnknown = true;
+
+console.log('Value of variableUnknown is:', variableUnknown);
