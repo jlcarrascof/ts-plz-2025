2c07e670f9e21762cca8e40403b3f0eb617ef787
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-13 12:14:52 -0400

    Add ages array and console log to lists.js and lists.ts

--- lists/lists.js
+++ lists/lists.js
@@ -1,2 +1,4 @@
 var names = ["Amin", "Marce", "Miranda"];
 console.log("Names List:", names);
+var ages = [39, 25, 30];
+console.log("Ages List:", ages);
--- lists/lists.ts
+++ lists/lists.ts
@@ -1,3 +1,5 @@
 let names: string[] = ["Amin", "Marce", "Miranda"];
 console.log("Names List:", names);
 
+let ages: number[] = [39, 25, 30];
+console.log("Ages List:", ages);
