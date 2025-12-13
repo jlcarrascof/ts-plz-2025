c49059d953b4f4f376581ae7563de84d172b7334
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-13 12:30:18 -0400

    Add people array and console logs to lists.js

--- lists/lists.js
+++ lists/lists.js
@@ -4,3 +4,11 @@ var ages = [39, 25, 30];
 console.log("Ages List:", ages);
 var mixedList = ["Amin", 39, true];
 console.log("Mixed List:", mixedList);
+var people = [
+    { name: "Amin", age: 39, isDeveloper: true },
+    { name: "Juan", age: 25, isDeveloper: false },
+    { name: "Maria", age: 30, isDeveloper: true },
+];
+console.log("People List:", people);
+people.push({ name: "Miranda", age: 30, isDeveloper: true });
+console.log("People List:", people);
