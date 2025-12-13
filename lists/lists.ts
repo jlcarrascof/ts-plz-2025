586e2d1a7c99cf9dca691b69faf9f6e58fc2dbe9
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-13 12:29:26 -0400

    Add people array and console log to lists.ts

--- lists/lists.ts
+++ lists/lists.ts
@@ -13,3 +13,13 @@ interface Person {
   isDeveloper: boolean;
 }
 
+let people: Person[] = [
+  { name: "Amin", age: 39, isDeveloper: true },
+  { name: "Juan", age: 25, isDeveloper: false },
+  { name: "Maria", age: 30, isDeveloper: true },
+];
+console.log("People List:", people);
+
+people.push({ name: "Miranda", age: 30, isDeveloper: true });
+console.log("People List:", people);
+
