6ba499851ba671bbc5d0786a56c35a8f9e84a6f0
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-24 12:57:53 -0400

    Add addAll function to sum multiple numbers and log the result

--- methods/methods2.ts
+++ methods/methods2.ts
@@ -8,3 +8,11 @@ function greeting(name: string, greet?: string): string {
 
 console.log(greeting('Amin'));
 console.log(greeting('Amin', 'Good morning'));
+
+// Multiple params .....
+
+function addAll(...numbers: number[]): number {
+    return numbers.reduce((accumulate, actual) => accumulate + actual, 0);
+}
+
+console.log(`Result of adding all numbers: ${addAll(1,2,3,4,5)}`);
