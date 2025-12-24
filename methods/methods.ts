e5ef94aa0f3ce4d8e2e7f1841671435d8901a77f
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-24 10:09:53 -0400

    Refactor methods.ts to include add function and log result

--- methods/methods.ts
+++ methods/methods.ts
@@ -3,3 +3,10 @@ function printMessage(message: string): void {
 }
 
 printMessage(`Hi, I'm a message`);
+
+function add(number1: number, number2: number) {
+    return number1 + number2;
+}
+
+let result: number = add(5, 10);
+console.log(`Your result is: ${result}`);
