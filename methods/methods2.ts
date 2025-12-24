67996d3e52f23668ffb87989a8c9f91cf7fe66fd
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-24 13:15:15 -0400

    Add sayGoodbye function with default parameter and log results

--- methods/methods2.ts
+++ methods/methods2.ts
@@ -16,3 +16,11 @@ function addAll(...numbers: number[]): number {
 }
 
 console.log(`Result of adding all numbers: ${addAll(1,2,3,4,5)}`);
+
+// Default values ....
+function sayGoodbye(name: string, goodbye: string = "Goodbye"): string {
+    return `${goodbye}, ${name}`;
+}
+
+console.log(sayGoodbye('Amin'));
+console.log(sayGoodbye('Amin', 'See ya later'));
