19839da82b28f6ce4ae36d1d5591b1f866d70a18
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-24 12:58:35 -0400

    Add addAll function to sum multiple numbers and log the result

--- methods/methods2.js
+++ methods/methods2.js
@@ -8,3 +8,12 @@ function greeting(name, greet) {
 }
 console.log(greeting('Amin'));
 console.log(greeting('Amin', 'Good morning'));
+// Multiple params .....
+function addAll() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (accumulate, actual) { return accumulate + actual; }, 0);
+}
+console.log("Result of adding all numbers: ".concat(addAll(1, 2, 3, 4, 5)));
