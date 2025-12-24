1fd4c4085c244418b0a8127c99fbcc58fb147554
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-24 10:10:16 -0400

    Add add function and log result in methods.js

--- methods/methods.js
+++ methods/methods.js
@@ -2,3 +2,8 @@ function printMessage(message) {
     console.log(message);
 }
 printMessage("Hi, I'm a message");
+function add(number1, number2) {
+    return number1 + number2;
+}
+var result = add(5, 10);
+console.log("Your result is: ".concat(result));
